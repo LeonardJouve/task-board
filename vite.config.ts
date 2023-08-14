600b698336873f40a12ed0fe7133b2dc0c1ba4bb
Léonard Jouve <leonard.jouve@gmail.com>
2023-08-14 15:51:25 +0200

    use vite resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+const dir = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [react({
-        babel: {
-            plugins: [
-                ["module-resolver", {
-                    "root": ["."],
-                    "alias": {
-                        "@components": "./src/components",
-                        "@typing": "./src/types",
-                        "@store": "./src/store",
-                    },
-                }],
-            ],
+    plugins: [react()],
+    root: "./",
+    resolve: {
+        alias: {
+            "@components": path.resolve(dir, "src/components"),
+            "@store": path.resolve(dir, "src/store"),
         },
-    })],
+    },
 });
